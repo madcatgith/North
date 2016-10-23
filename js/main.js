2c1417a4b737210d6c54b88ab701dcff1b492549
Redplek <Andreydeyneko1994@gmail.com>
2016-10-23 23:37:02 +0300

    Fucking slider

--- js/main.js
+++ js/main.js
@@ -3,4 +3,31 @@ $(document).ready(function(){
 		e.preventDefault();
 		$('#file').click();
 	})
+	$('.center').slick({
+	  centerMode: true,
+	  centerPadding: '60px',
+ 	 slidesToShow: 2,
+ 	 dots: true,
+ 	 arrows: false,
+	  responsive: [
+ 	   {
+ 	     breakpoint: 768,
+ 	     settings: {
+  	      arrows: false,
+  	      centerMode: true,
+  	      centerPadding: '40px',
+  	      slidesToShow: 2
+  	    }
+  	  },
+  	  {
+  	    breakpoint: 480,
+  	    settings: {
+  	      arrows: false,
+  	      centerMode: true,
+  	      centerPadding: '40px',
+  	      slidesToShow: 2
+  	    }
+ 	   }
+	  ]
+	});
 });
